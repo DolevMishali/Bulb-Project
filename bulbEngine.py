808a396347eccf4c2f04cb70595fc6edd1577f87
DolevMishali <dolevmi2010@gmail.com>
2023-04-06 15:56:10 +0300

    start db

--- bulbEngine.py
+++ bulbEngine.py
@@ -1,16 +1,31 @@
+from yeelight import discover_bulbs
 from yeelight import Bulb
-import random, sys
 
-args = {}
 
-def argsParser():
-    numOfArgs = sys.argv.__len__()
-    for i in range(1, numOfArgs, 2):
-        flag = sys.argv[i]
-        flag_value = sys.argv[i+1]
-        args[flag] = flag_value
+
+class BulbEngine:
+    def __init__(self):
+        bulb = db.get_bulb()
+        if bulb is not None:
+            self.bulb = Bulb(bulb.ip)
+            self.bulb_data = self.bulb.get_capabilities()
+
+    def discover(self):
+        self.bulb_data = discover_bulbs()[0]
+        self.bulb = Bulb(self.bulb_data.ip)
         
-argsParser()
-
-bulb = Bulb(args["--ip"])
-bulb.set_rgb(int(args["-r"]), int(args["-g"]), int(args["-b"]))
+    def set_brightness(brightness):
+        if self.bulb is not None:
+            self.bulb.set_brightness(brightness)
+    
+    def set_color_temp(temperature):
+        if self.bulb is not None:
+            self.bulb.set_color_temp(temperature)
+            
+    # def set_color(rgb):
+    #     if self.bulb is not None:
+    #         self.bulb.set_color_temp(temperature)
+            
+    # def set_color(hex):
+    #     if self.bulb is not None:
+    #         self.bulb.set_color_temp(temperature)
--- yeelightWrapper.py
+++ yeelightWrapper.py
@@ -1,17 +1,41 @@
+from yeelight import discover_bulbs
 from yeelight import Bulb
+
+import sounddevice as sd
+import numpy as np
+
 import random, sys
 
+bulb_data = discover_bulbs()                                    # The data of the bulb
+bulb_ip_address=bulb_data[0]["ip"]                              # The ip of the bulb
+bulb_support_methods = bulb_data[0]["capabilities"]["support"]  # The support methods that the bulb is support
+bulb_name = bulb_data[0]["capabilities"]["name"]                # The name of the bulb
+bulb = Bulb(bulb_ip_address)                                    # Set the ip into variable
 
-#bulb = Bulb("10.0.0.8")
-#
+cap = bulb.get_capabilities()
+
+# Check microphone effect
+def audio_callback(indata, frames, time, status):
+    amplitude = np.max(indata)
+    brightness = int(amplitude * 100)
+    random_numberR = random.randint(1, 255)
+    random_numberG = random.randint(1, 255)
+    random_numberB = random.randint(1, 255)
+    bulb.set_rgb(random_numberR, random_numberG, random_numberB)
+    bulb.set_brightness(brightness)
+
+with sd.InputStream(callback=audio_callback):
+    sd.sleep(60000)
+# Check microphone effect
+
+
 #def sleep():
 #    for i in range(1, 10000):
 #        print()
-#        
+#                
 #for i in range(1, 10):
 #    random_numberR = random.randint(1, 255)
 #    random_numberG = random.randint(1, 255)
 #    random_numberB = random.randint(1, 255)
 #    bulb.set_rgb(random_numberR, random_numberG, random_numberB)
 #    sleep() 
-
