9feb05888315bc80fca3d10833622f1f74723bbe
DolevMishali <dolevmi2010@gmail.com>
2023-04-11 17:44:55 +0300

    html and css updated and func connected with the api

--- client/client.js
+++ client/client.js
@@ -1,9 +1,113 @@
 const apiUrl = 'http://localhost:5000';
+const apiBulbRoute = '/bulb';
 
 let timerId;
+let intervalId;
+let uichanging = false;
+let uichangingtimerId;
+
+
+function init() {
+  intervalId = setInterval(async () => {
+    if(uichanging)
+      return
+
+    const response = await fetch(`${apiUrl}${apiBulbRoute}/props`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    const json = await response.json();
+
+    if(!json) {
+      //clearInterval(intervalId)
+      return
+    }
+      
+    setColorPicker(json);
+    setPower(json);
+    setBrightness(json);
+
+  }, 500);
+}
+
+//Update the UI
+
+function setBrightness(json) {
+  const brightness = document.getElementById("brightness");
+  brightness.value = json.bright
+}
+
+function setPower(json) {
+  const power = document.getElementById("bulbpower");
+  power.checked = json.power === 'on';
+}
+
+function setColorPicker(json) {
+  const colorpicker = document.getElementById("colorpicker");
+  color_int = parseInt(json.rgb);
+
+  var binaryStr = color_int.toString(2).padStart(24, '0');
+  var hexStr = '';
+
+  for (var i = 0; i < 3; i++) {
+    var byte = binaryStr.substr(i * 8, 8);
+    var hexByte = parseInt(byte, 2).toString(16).padStart(2, '0');
+    hexStr += hexByte;
+  }
+
+  colorpicker.value = "#" + hexStr;
+}
+
+
+//Requests from server
+
+
+function brightnessChanged(event) {
+  event.preventDefault();
+  UIChanged()
+
+  if(!timerId) {
+    timerId = setTimeout(async () => {
+    fetch(`${apiUrl}${apiBulbRoute}/brightness`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        value: event.target.valueAsNumber
+      })
+    });
+  
+    timerId = null;
+  }, 200)
+
+  //Dont change the UI !!
+  //event.target.value = !event.target.checked
+  }
+}
+
+function powerchanged(event) {
+  event.preventDefault();
+ 
+  fetch(`${apiUrl}${apiBulbRoute}/power`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      status: event.target.checked
+    })
+  });
+
+  //Dont change the UI !!
+  event.target.checked = !event.target.checked
+}
 
 async function setRgbValues(red, green, blue) {
-  const response = await fetch(`${apiUrl}/set_rgb_by_values`, {
+  const response = await fetch(`${apiUrl}${apiBulbRoute}/color_by_rgb`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -19,16 +123,23 @@ async function setRgbValues(red, green, blue) {
   console.log(json.message);
 }
 
-function turnPower(status) {
-  fetch(`${apiUrl}/power/${status ? 'turn_on' : 'turn_off'}`, {
-    method: 'GET'
+async function turnPower(status) {
+  await fetch(`${apiUrl}${apiBulbRoute}/power`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      status: status
+    })
   });
 }
 
 async function setHexColor(color) {
     if(!timerId) {
+      UIChanged()
     timerId = setTimeout(async () => {
-      await fetch(`${apiUrl}/set_color_by_hex`, {
+      await fetch(`${apiUrl}${apiBulbRoute}/color_by_hex`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -41,4 +152,14 @@ async function setHexColor(color) {
       timerId = null;
     }, 50)
   }
+}
+
+//Misc
+function UIChanged() {
+  uichanging = true
+  if(!uichangingtimerId)
+  uichangingtimerId = setTimeout(() => {
+    uichanging = false
+    uichangingtimerId = null
+  }, 500);
 }
