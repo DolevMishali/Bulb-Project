795507870944049325b791588812554dc59e36c4
DolevMishali <dolevmi2010@gmail.com>
2023-05-04 19:59:57 +0300

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavbar from './components/MyNavbar'
-import Main from './components/main'
+import MyNavbar from './components/MyNavbar';
+import Main from './components/main';
 
 function App() {
-  return (// Header, Data components, footer 
-  <div className='App'>
+  return (
+    // Header, Data components, footer
+    <div className='App'>
       <MyNavbar />
-      <Main />  
-  </div>
+      <Main />
+    </div>
   );
 }
 
