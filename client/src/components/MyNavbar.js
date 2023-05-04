4f6ba49c2c0192d5b9e63796e1566c4f74157eab
DolevMishali <dolevmi2010@gmail.com>
2023-05-05 00:52:16 +0300

    Navbar css file was created

--- client/src/components/MyNavbar.js
+++ client/src/components/MyNavbar.js
@@ -1,22 +1,13 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { BiBulb } from 'react-icons/bi';
+import './MyNavbar.css';
 
 function MyNavbar() {
-  const navbarBrandStyle = {
-    letterSpacing: '1em',
-    fontSize: '150%',
-    background:
-      'linear-gradient(90deg, rgba(58,139,180,1) 0%, rgba(255,255,255,1) 100%)',
-    '-webkit-background-clip': 'text' /* Add this for Safari compatibility */,
-    '-webkit-text-fill-color':
-      'transparent' /* Add this for Safari compatibility */,
-  };
-
   return (
     <Navbar bg='dark' variant='dark'>
       <Container>
-        <Navbar.Brand style={navbarBrandStyle} className='mx-auto'>
+        <Navbar.Brand className='navbarStyle'>
           <BiBulb style={{ marginRight: 50, color: '#FFC107' }} />
           Yeelight Bulb Controller
         </Navbar.Brand>
