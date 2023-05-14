80a24c7234aacbc254f533e688e4db01a067f2e4
DolevMishali <dolevmi2010@gmail.com>
2023-05-14 16:06:43 +0300

    colorPicker logic was added

--- client/src/components/ColorPicker.js
+++ client/src/components/ColorPicker.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import { HuePicker } from 'react-color';
 
-class ColorPicker extends React.Component {
-  render() {
-    return <HuePicker width='200px' />;
-  }
-}
+const ColorPicker = ({ onChange }) => {
+  // Destructuring the 'onChange' prop from the component's props
+  const [color, setColor] = useState(); // State hook to manage the 'color' state
+
+  const changeHandler = (color) => {
+    // Event handler function that takes in a 'color' parameter
+    setColor(color); // Updates the 'color' state with the new value
+    onChange(color.hex); // Calls the 'onChange' prop with the hex value of the color
+  };
+
+  // Render the HuePicker component from a color picker library
+  return <HuePicker width='200px' color={color} onChange={changeHandler} />;
+};
 
 export default ColorPicker;
-//Todo - Add onChange
+
 // options of pickers https://casesandberg.github.io/react-color/
